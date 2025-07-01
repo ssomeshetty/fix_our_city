d83b77b48734c911f4dd845cfceb4b9963967465
ssomeshetty <bremblue189@gmail.com>
2025-07-01 11:42:29 +0530

    tesed

--- fixourcity/public/tests.py
+++ fixourcity/public/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras import layers, Model
 
-# Create your tests here.
+base = EfficientNetB0(include_top=False, weights='imagenet', pooling='avg')
+base.trainable = False
+
+inp = layers.Input(shape=(224, 224, 3))
+x = base(inp, training=False)
+x = layers.BatchNormalization()(x)
+x = layers.Dropout(0.3)(x)
+out = layers.Dense(2, activation='softmax')(x)
+
+model = Model(inp, out)
+model.compile(optimizer='adam', loss=focal_loss, metrics=['accuracy', 'AUC']) 
